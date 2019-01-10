228c8d1328ef4b25b68fd8d5bf8cbf3a0b8582d0
Noman Afzal <noman.afzal@ucp.edu.pk>
2019-01-10 17:52:01 +0500

    Regular Expression Activity

--- pro3/register.php
+++ pro3/register.php
@@ -106,7 +106,7 @@ require "server/functions.php";
                             <div class="input-group-text"><i class="fas fa-user"></i></div>
                         </div>
                         <input type="text" class="form-control" id="c_name" name="c_name" placeholder="Enter your name"
-                               required pattern="abc">
+                               required pattern="0?[1-9]|1[0-9]|2[0-9]|3[01]">
                     </div>
                 </div>
                 <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-2 mt-auto">
@@ -130,7 +130,8 @@ require "server/functions.php";
                         <div class="input-group-prepend">
                             <div class="input-group-text"><i class="fas fa-key"></i></div>
                         </div>
-                        <input type="password" class="form-control" id="c_password" name="c_password" placeholder="Enter your password" >
+                        <input type="text" class="form-control" id="c_password" name="c_password" placeholder="Enter your password" 
+                        required pattern="19[5-9][0-9]|200[01]">
                     </div>
                 </div>
                 <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-2 mt-auto">
